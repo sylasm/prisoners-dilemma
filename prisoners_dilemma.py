b893941fe9ab6f03a8300d74cf944a783af44de6
RGoldman <41007661+RaeGoldman@users.noreply.github.com>
2019-02-04 09:13:59 -0600

    temp fix

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -36,14 +36,14 @@ import os.path
 import example0, example1, example2, example3
 import example4, example5, example6, example7
 import team0, team1, team2, team3, team4
-import team5, team6, team7, team8, team9
-import team10, team11, team12, team13, team14
+import team5, team6, team7, team8, team9, team10
+#import team10, team11, team12, team13, team14
 betray = example1
 collude = example0
 
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
-team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
-team11, team12, team13, team14]
+team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10]
+#team11, team12, team13, team14]
 for module in modules:
     reload(module)
     print ('reloaded',module)
